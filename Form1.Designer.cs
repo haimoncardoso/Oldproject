f6b4484cf4faa8e3a46afc0ef39390588f8cf26e
haimoncardoso <bexliuscoc@gmail.com>
2021-03-02 18:59:49 -0300

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Projeto_Idade
+{
+    partial class FrmProjetoIdade
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmProjetoIdade));
+            this.TxtIdade1 = new System.Windows.Forms.TextBox();
+            this.TxtIdade2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Txtnome1 = new System.Windows.Forms.TextBox();
+            this.Txtnome2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtAno = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.PicFechar = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PicFechar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TxtIdade1
+            // 
+            this.TxtIdade1.Enabled = false;
+            this.TxtIdade1.Location = new System.Drawing.Point(465, 111);
+            this.TxtIdade1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TxtIdade1.Name = "TxtIdade1";
+            this.TxtIdade1.Size = new System.Drawing.Size(132, 23);
+            this.TxtIdade1.TabIndex = 0;
+            this.TxtIdade1.TextChanged += new System.EventHandler(this.Txtdatadenascimento1_TextChanged);
+            this.TxtIdade1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtIdade1_KeyPress);
+            // 
+            // TxtIdade2
+            // 
+            this.TxtIdade2.Enabled = false;
+            this.TxtIdade2.Location = new System.Drawing.Point(465, 221);
+            this.TxtIdade2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TxtIdade2.Name = "TxtIdade2";
+            this.TxtIdade2.Size = new System.Drawing.Size(132, 23);
+            this.TxtIdade2.TabIndex = 0;
+            this.TxtIdade2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtIdade2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(89, 224);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(89, 113);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(326, 222);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Ano De Nascimento:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Location = new System.Drawing.Point(326, 113);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Ano de Nascimento:";
+            // 
+            // Txtnome1
+            // 
+            this.Txtnome1.Location = new System.Drawing.Point(140, 107);
+            this.Txtnome1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Txtnome1.Name = "Txtnome1";
+            this.Txtnome1.Size = new System.Drawing.Size(132, 23);
+            this.Txtnome1.TabIndex = 0;
+            this.Txtnome1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Txtnome1_KeyPress);
+            // 
+            // Txtnome2
+            // 
+            this.Txtnome2.Enabled = false;
+            this.Txtnome2.Location = new System.Drawing.Point(140, 222);
+            this.Txtnome2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Txtnome2.Name = "Txtnome2";
+            this.Txtnome2.Size = new System.Drawing.Size(132, 23);
+            this.Txtnome2.TabIndex = 0;
+            this.Txtnome2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Txtnome2_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(589, 300);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Digite o Ano Atual:";
+            // 
+            // TxtAno
+            // 
+            this.TxtAno.Enabled = false;
+            this.TxtAno.Location = new System.Drawing.Point(592, 322);
+            this.TxtAno.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TxtAno.Name = "TxtAno";
+            this.TxtAno.Size = new System.Drawing.Size(132, 23);
+            this.TxtAno.TabIndex = 0;
+            this.TxtAno.TextChanged += new System.EventHandler(this.Txtdatadenascimento1_TextChanged);
+            this.TxtAno.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtAno_KeyPress);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SeaShell;
+            this.panel1.Location = new System.Drawing.Point(81, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 239);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.SeaShell;
+            this.panel2.Location = new System.Drawing.Point(317, 54);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 239);
+            this.panel2.TabIndex = 2;
+            // 
+            // PicFechar
+            // 
+            this.PicFechar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PicFechar.Image = ((System.Drawing.Image)(resources.GetObject("PicFechar.Image")));
+            this.PicFechar.Location = new System.Drawing.Point(678, 1);
+            this.PicFechar.Name = "PicFechar";
+            this.PicFechar.Size = new System.Drawing.Size(46, 34);
+            this.PicFechar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PicFechar.TabIndex = 3;
+            this.PicFechar.TabStop = false;
+            this.PicFechar.Click += new System.EventHandler(this.PicFechar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(140, 293);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(329, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(43, 34);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PicFechar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Maroon;
+            this.label6.Location = new System.Drawing.Point(40, 7);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 17);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Haimondesouza";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(-1, 279);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(135, 119);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.PicFechar_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(606, 122);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(107, 119);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.PicFechar_Click);
+            // 
+            // FrmProjetoIdade
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(725, 384);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.PicFechar);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Txtnome2);
+            this.Controls.Add(this.Txtnome1);
+            this.Controls.Add(this.TxtIdade2);
+            this.Controls.Add(this.TxtAno);
+            this.Controls.Add(this.TxtIdade1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "FrmProjetoIdade";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Projeto Idades";
+            this.Load += new System.EventHandler(this.FrmProjetoIdade_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.FrmProjetoIdade_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.PicFechar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtIdade1;
+        private System.Windows.Forms.TextBox TxtIdade2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Txtnome1;
+        private System.Windows.Forms.TextBox Txtnome2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtAno;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox PicFechar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Idade
+{
+    public partial class FrmProjetoIdade : Form
+    {
+        string nome1, nome2;
+        int nascimento1, nascimento2, ano_atual, idade1, idade2;
+
+        private void Txtdatadenascimento1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Txtnome2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar ==13)
+            {
+                if (Txtnome2.Text != "")
+                {
+                    nome2 = Txtnome2.Text;
+                    TxtIdade2.Enabled = true;
+                    TxtIdade2.Focus();
+                }
+                else
+                    MessageBox.Show("Você não digitou o Nome", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) &&  !(e.KeyChar == (char)Keys.Back) && !(e.KeyChar == (char)Keys.Space)) 
+            {
+                e.Handled = true;
+                MessageBox.Show("Somente Letras", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void TxtIdade1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                if(TxtIdade1.Text != "")
+                {
+                    nascimento1 = int.Parse(TxtIdade1.Text);
+                    Txtnome2.Enabled = true;
+                    Txtnome2.Focus();
+
+                }
+                else
+                    MessageBox.Show("Você não digitou o ano de nascimento", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            if (!char.IsControl(e.KeyChar) && !char.IsNumber(e.KeyChar))
+            {
+               e.Handled = true;
+               MessageBox.Show("Não é permitido digitar letras!!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void TxtIdade2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar ==13)
+            {
+                if(TxtIdade2.Text != "")
+                {
+                    nascimento2 = int.Parse(TxtIdade2.Text);
+                    TxtAno.Enabled = true;
+                    TxtAno.Focus();
+                }
+                else
+                    MessageBox.Show("Você não digitou o ano de nascimento", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (!char.IsControl(e.KeyChar) && !char.IsNumber(e.KeyChar))
+            {
+               e.Handled = true;
+               MessageBox.Show("Não é permitido Digitar Letras!!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void TxtAno_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar ==13)
+            {
+                if (TxtAno.Text !="")
+                {
+                    ano_atual = int.Parse(TxtAno.Text);
+
+                    idade1 = ano_atual - nascimento1;
+                    idade2 = ano_atual - nascimento2;
+
+                    if ((ano_atual > nascimento1) && (ano_atual > nascimento2)) //O Ano tem que ser sempre Maior
+                    {
+                        if (idade1 > idade2)
+                        {
+                            MessageBox.Show(nome1 + " É mais Velho(a) com" + idade1.ToString() + "anos." + "\n" + "\n" + nome2 + " é mais novo(a) com" + idade2.ToString() + " anos.", "Resultado", MessageBoxButtons.OK);
+                        }
+                        else if (idade1 < idade2)
+                        {
+                            MessageBox.Show(nome2 + " É mais velho(a) com" + idade2.ToString() + "anos." + "\n" + "\n" + nome1 + " é mais novo(a) com" + idade1.ToString() + " anos", "Resultado", MessageBoxButtons.OK);
+                        }
+                        else
+                        {
+                            MessageBox.Show(nome1 +" E " + nome2+" São Gemeos", "Resultado", MessageBoxButtons.OK);
+                        }
+                    }
+
+                }
+            }
+           if (!char.IsControl(e.KeyChar) && !char.IsNumber(e.KeyChar))
+            {
+               e.Handled = true;
+               MessageBox.Show("Não é permitido digitar letras!!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        } //Ano atual - o Ano de Nascimento
+
+        private void FrmProjetoIdade_KeyPress(object sender, KeyPressEventArgs e)
+        {
+         
+        }
+
+        private void PicFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Txtnome1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                if (Txtnome1.Text != "")
+                {
+                    nome1 = Txtnome1.Text;
+                    TxtIdade1.Enabled = true;
+                    TxtIdade1.Focus();
+                }
+                else
+                    MessageBox.Show("você não digitou o Nome", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) &&  !(e.KeyChar ==(char)Keys.Back) &&  !(e.KeyChar ==(char) Keys.Space))
+                
+            {
+              e.Handled = true;
+              MessageBox.Show("Somente Letras", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public FrmProjetoIdade()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmProjetoIdade_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
